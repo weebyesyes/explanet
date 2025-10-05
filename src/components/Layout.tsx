6cc4a9aefc5535d41f842a43aeab1ce5e141c60f
whiteblobbob <whiteblobbob@gmail.com>
2025-10-06 00:16:35 +0800

    planet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/components/Layout.module.css";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
+const planetImages = [
+  require("../../public/images/planet1.png"),
+  require("../../public/images/planet2.png"),
+  require("../../public/images/planet3.png"),
+  require("../../public/images/planet4.png"),
+]
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [sidebarOpened, setSidebarOpened] = useState(false);
+  const [mouse, setMouse] = useState({x: 0, y: 0});
   const router = useRouter();
 
   function SidebarButton(props: { label: string, destination: string }) {
@@ -15,8 +23,68 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     )
   }
 
+  const [planets, setPlanets] = useState<any[]>([]);
+
+  useEffect(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const planets: any[] = [];
+    const grids = 7;
+
+    const usedX: number[] = [];
+    const usedY: number[] = [];
+
+    for (let i = 0; i < 4; i++) {
+      let randomX = Math.floor(Math.random() * grids) - 1;
+      let randomY = Math.floor(Math.random() * grids) - 1;
+
+      while (usedX.includes(randomX) || usedY.includes(randomY)) {
+        randomX = Math.floor(Math.random() * grids) - 1;
+        randomY = Math.floor(Math.random() * grids) - 1;
+      }
+
+      usedX.push(randomX);
+      usedY.push(randomY);
+
+      // grids
+      planets[i] = {};
+      planets[i].src =  planetImages[i];
+      planets[i].left = randomX * Math.floor(width / grids);
+      planets[i].top = randomY * Math.floor(height / grids);
+      planets[i].alt = i.toString();
+    }
+
+    setPlanets(planets);
+
+    const handleMouseMove = (e: MouseEvent) => {
+      const x = e.clientX * 17 / width;
+      const y = e.clientY * 17 / height;
+      setMouse({ x, y });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, [])
+
   return (
     <div className={styles.container}>
+      <div className={styles.planets}>
+        {planets.map(planet => (
+          <Image
+            key={`planet${planet.alt}`}
+            src={planet.src}
+            alt={planet.alt}
+            width={320}
+            style={{
+              position: 'absolute',
+              left: planet.left + mouse.x,
+              top: planet.top + mouse.y,
+              filter: 'brightness(50%)',
+              zIndex: -1
+            }}
+          />
+        ))}
+      </div>
+
       <div className={styles.content}>
         {children}
       </div>
